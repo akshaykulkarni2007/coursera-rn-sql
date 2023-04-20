c94e7c3c21633837eba85950b0290057a626eb73
Akshay Kulkarni <akshaykulkarni2007@gmail.com>
2023-04-20 19:59:22 +0530

    C06W02E02: local fetch

--- App.js
+++ App.js
@@ -1,28 +1,20 @@
 import 'react-native-gesture-handler'
 
-import { StyleSheet } from 'react-native'
 import { NavigationContainer } from '@react-navigation/native'
 import { createStackNavigator } from '@react-navigation/stack'
 
 import { MenuScreen } from './Screens/MenuScreen'
+import { MenuScreenLocalFetch } from './Screens/MenuScreenLocalFetch'
 
 const Stack = createStackNavigator()
 
 export default function App() {
 	return (
 		<NavigationContainer>
-			<Stack.Navigator initialRouteName="Menu">
+			<Stack.Navigator initialRouteName="MenuLocal">
 				<Stack.Screen name="Menu" component={MenuScreen} />
+				<Stack.Screen name="MenuLocal" component={MenuScreenLocalFetch} />
 			</Stack.Navigator>
 		</NavigationContainer>
 	)
 }
-
-const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		backgroundColor: '#fff',
-		alignItems: 'center',
-		justifyContent: 'center',
-	},
-})
