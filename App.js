887f1d5736151f35e6dbf7cfb6627402c3bd1ef2
Akshay Kulkarni <akshaykulkarni2007@gmail.com>
2023-04-22 02:06:12 +0530

    C06W04E01: async storage

--- App.js
+++ App.js
@@ -5,15 +5,17 @@ import { createStackNavigator } from '@react-navigation/stack'
 
 import { MenuScreen } from './Screens/MenuScreen'
 import { MenuScreenLocalFetch } from './Screens/MenuScreenLocalFetch'
+import { PreferencesAsyncStorage } from './Screens/PreferencesAsyncStorage'
 
 const Stack = createStackNavigator()
 
 export default function App() {
 	return (
 		<NavigationContainer>
-			<Stack.Navigator initialRouteName="MenuLocal">
+			<Stack.Navigator initialRouteName="Preferences">
 				<Stack.Screen name="Menu" component={MenuScreen} />
 				<Stack.Screen name="MenuLocal" component={MenuScreenLocalFetch} />
+				<Stack.Screen name="Preferences" component={PreferencesAsyncStorage} />
 			</Stack.Navigator>
 		</NavigationContainer>
 	)
