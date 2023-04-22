1d6199d1b085e7500de8b4967cbfc23630972ceb
Akshay Kulkarni <akshaykulkarni2007@gmail.com>
2023-04-22 13:17:01 +0530

    C06W04E02: SQLite

--- App.js
+++ App.js
@@ -6,16 +6,18 @@ import { createStackNavigator } from '@react-navigation/stack'
 import { MenuScreen } from './Screens/MenuScreen'
 import { MenuScreenLocalFetch } from './Screens/MenuScreenLocalFetch'
 import { PreferencesAsyncStorage } from './Screens/PreferencesAsyncStorage'
+import { CustomerSQLite } from './Screens/CustomerSQLite'
 
 const Stack = createStackNavigator()
 
 export default function App() {
 	return (
 		<NavigationContainer>
-			<Stack.Navigator initialRouteName="Preferences">
+			<Stack.Navigator initialRouteName="Customers">
 				<Stack.Screen name="Menu" component={MenuScreen} />
 				<Stack.Screen name="MenuLocal" component={MenuScreenLocalFetch} />
 				<Stack.Screen name="Preferences" component={PreferencesAsyncStorage} />
+				<Stack.Screen name="Customers" component={CustomerSQLite} />
 			</Stack.Navigator>
 		</NavigationContainer>
 	)
